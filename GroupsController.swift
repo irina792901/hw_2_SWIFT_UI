bbceb5d58da590eeb8f3a1000311050fc7ea44dc
Ирина Шипунова <108937167+irina792901@users.noreply.github.com>
2023-07-19 20:22:00 +0700

    Create GroupsController.swift

---
+++ GroupsController.swift
@@ -0,0 +1,34 @@
+import UIKit
+class GroupsController: UIViewCOntroller, UITableViweController {
+  private var tableView = UITableView(frame: .zero, style: .grouped)
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      5
+    }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nill)
+      cell.textLabel?.text = "Hello"
+      cell.imageView?.image = UIImage(systemName: "person")
+      return cell
+    }
+  
+ 
+  func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    tableView.dataSource = self
+    tableView.delegate = self
+    view.addSubview(tableView)
+    setupConstraints()
+  }
+  func setupConstraints() {
+    tableView.translateAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnhor),
+      tableView.bottomAnchor.constraint(equalTo: wiew.safeAreaLayoutGuide.bottomAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+  }
+}
